d9bd34dfdfca942d034eb26c56a2844da2f498d7
LeGabrielMS <gabrielmarcellino77@gmail.com>
2024-06-07 20:08:32 +0700

    Menambahkan Groundwork Untuk Firebase Analytics Dan Firestore Database

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,8 @@ dependencies {
     implementation(libs.androidx.lifecycle.livedata.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.firebase.auth)
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.analytics)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
